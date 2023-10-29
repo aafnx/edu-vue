40042cbfd38cdac86444957fd8525eeaef081bcc
aa <aa@fnx.red>
2023-10-29 15:32:12 +0300

    save progress

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
+    <ProductDetails />
   </div>
 </template>
 
 <script>
+import ProductDetails from '@/components/ProductDetails.vue';
+
 export default {
   name: 'App',
   components: {
+    ProductDetails,
   },
 };
 </script>
---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: 'ProductDetails',
+  data() {
+    return {
+      product: {
+        name: 'Mock',
+        price: 1000,
+        available: true,
+      },
+    };
+  },
+};
+</script>
+
+<template>
+<h1>hi</h1>
+</template>
+
+<style scoped>
+
+</style>
